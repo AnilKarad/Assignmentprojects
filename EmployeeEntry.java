d67dc0be3d4376bd9bbb0591d8d593162ad769a4
AnilKarad <122225641+AnilKarad@users.noreply.github.com>
2023-01-11 19:00:34 +0530

    Add files via upload

---
+++ EmployeeEntry.java
@@ -0,0 +1,40 @@
+package EmployEntry;
+public class EmployeeEntry {
+	private int id;
+	private String name;  
+	private int age;  
+	private String dept;
+
+	public EmployeeEntry(){}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String Dept) {
+		this.dept = dept;
+	}  	
+
+}
